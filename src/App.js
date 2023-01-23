f8e3827901b35cc0c6950b4bb09a958a49226084
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-01-23 19:45:49 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-
+const [desc, setDesc] = useState([])
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+},[])
   return (
-    <div className="App">
-      <h1>test</h1>
+    <div className='bg-sky-500'>
+      <h1 >test</h1>
     </div>
   );
 }
